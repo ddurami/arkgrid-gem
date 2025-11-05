fe9687fe52e484f18bf80da96d2c2e6f435150c8
ddurami9323 <ddurami9323@gmail.com>
2025-11-05 09:55:14 +0900

    흰 화면 대응

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isProduction = process.env.NODE_ENV === "production";
+const basePath = isProduction ? "/arkgrid-gem/" : "/";
+
 export default defineConfig({
     plugins: [react()],
-    base: "/arkgrid-gem/",
+    base: basePath,
+    build: {
+        outDir: "docs",
+        emptyOutDir: true,
+    },
 });
